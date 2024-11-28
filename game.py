2cb1437854acc82e1bc2c39d9c42588de23e406d
MuhammadAKJ <Muhammadakj11@gmail.com>
2024-11-28 10:53:13 +0100

    ADDED[created another screen called Display and scaled + blit screen into display]

--- game.py
+++ game.py
@@ -13,6 +13,8 @@ class Game:
         # Set screen variable to a resolution of 640, 480
         self.screen = pygame.display.set_mode((640, 480))
 
+        self.display = pygame.Surface((320, 240))
+
         # Set screen title
         pygame.display.set_caption('ninga game')
 
@@ -30,10 +32,10 @@ class Game:
     def run(self):
         """Run game"""
         while True:
-            self.screen.fill((14, 219, 248)) #fill previous rendered screen
+            self.display.fill((14, 219, 248)) #fill previous rendered screen
          
             self.player.update((self.movement[0] - self.movement[1], 0))
-            self.player.render(self.screen)
+            self.player.render(self.display)
             # Loop to get input event either from the keyboard or the mouse
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -52,6 +54,8 @@ class Game:
                     if event.key == pygame.K_LEFT:
                         self.movement[1] = False
             
+
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()))
             # This line update all frame for every loop
             pygame.display.update()
             
